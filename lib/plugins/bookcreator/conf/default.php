57aaa9c344b8c6dfb248ddf891b7f150d8332023
root <root@cchum-epcc-projetsged-dev.in2p3.fr>
2019-03-28 16:47:23 +0100

    Ajout du plugin créant livre PDF

---
+++ lib/plugins/bookcreator/conf/default.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Options for the for the bookcreator Plugin
+ *
+ * @author  Luigi Micco <l.micco@tiscali.it>
+ */
+
+$conf['toolbar']         = 'noempty';
+
+$conf['book_page']       = 'condorcet:ebook'; 
+$conf['help_page']       = 'condorcet:ebook_help'; 
+$conf['save_namespace']  = 'condorcet:ebook'; 
+
+$conf['skip_ids']        = 'sidebar,user,group,playground,wiki:syntax,wiki:ebook';
+
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
