f04f29589ebb3076ebef5714b34fcf36be8bb819
root <root@cchum-epcc-projetsged-dev.in2p3.fr>
2019-03-28 15:29:33 +0100

    Commit initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# doc_pro_sgbm
+
+Dokuwiki contenant la documentation professionnelle pour le SGBm (ALMA) du GED Condorcet.
+
---
+++ conf/users.auth.php
@@ -0,0 +1,15 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+admin:$1$uWNfAHDZ$h3tcAimyDbgdFeCF9CQqn0:Administrateur Dokuwiki Alma - GED - Campus Condorcet:jerome.chiavassa-szenberg@campus-condorcet.fr:admin,user
+jchiavassa:$1$LvNUNBhQ$1VOPLtRo7wkVX41om9K5J/:Jérôme Chiavassa-Szenberg:jerome.chiavassa-szenberg@campus-condorcet.fr:admin,user
+nicomo:$1$v7rVty0m$uVvfgocBHq7pFMnD06Tj6/:Nicolas Morin:nicolas.morin@campus-condorcet.fr:admin,user
+rguidoni:$1$gnpmlFo0$262NsF3xbzn4JKyUNvhaK/:Rachel Guidoni:Rachel.GUIDONI@cnrs.fr:user
