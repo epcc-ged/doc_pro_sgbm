bda59ef0623a3c3c202bb83a883e033620f7da3a
root <root@cchum-epcc-projetsged-dev.in2p3.fr>
2019-04-26 13:13:03 +0200

    Sauvegarde 26/04 midi

---
+++ lib/plugins/authremoteuser/lang/en/lang.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ */
+$lang['stripOffDomain'] = 'Strip off the domain ("DOMAIN\") from User-ID';
+$lang['userexists']     = 'Sorry, a user with this login already exists.';
+$lang['usernotexists']  = 'Sorry, that user doesn\'t exist.';
+$lang['writefail']      = 'Unable to modify user data. Please inform the Wiki-Admin';
