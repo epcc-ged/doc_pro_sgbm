d2cdf8c98b032e7b54ade78838c1e2711e076d83
root <root@cchum-epcc-projetsged.in2p3.fr>
2020-02-27 09:05:38 +0100

    Sauvegarde 27/02/2020

--- conf/users.auth.php
+++ conf/users.auth.php
@@ -12,7 +12,7 @@
 admin:$1$uWNfAHDZ$h3tcAimyDbgdFeCF9CQqn0:Administrateur Dokuwiki Alma - GED - Campus Condorcet:jerome.chiavassa-szenberg@campus-condorcet.fr:admin,user
 jchiavassa:$1$LvNUNBhQ$1VOPLtRo7wkVX41om9K5J/:Jérôme Chiavassa-Szenberg:jerome.chiavassa-szenberg@campus-condorcet.fr:admin,user
 rguidoni:$1$gnpmlFo0$262NsF3xbzn4JKyUNvhaK/:Rachel Guidoni:Rachel.GUIDONI@cnrs.fr:user
-mhgatto:$1$oStBACpT$yrYyka96LKWySkeZCxcao1:Marie-Hélène Gatto:marie-helene.gatto@campus-condorcet.fr:user
 aalbespy:$1$i3kVfo8v$V4kuU9EOmkHTJdQrWKSVM.:Aurélie Albespy:aurelie.albespy@campus-condorcet.fr:user
 nicomo:$1$epNcEWCf$tDBHbibgYxJrdqx.WoLsK/:Nicolas Morin:nicolas.morin@campus-condorcet.fr:admin,user
 fmouillot:$1$SSkle2EZ$2s7YUpqJtLklFNiAKhiwd1:Fabrice Mouillot:fabrice.mouillot@campus-condorcet.fr:user
+mhgatto:$1$xa2eJeML$w1L3c9AgDeOF4l7E0DhJ5/:Marie-Hélène Gatto:marie-helene.gatto@campus-condorcet.fr:user
